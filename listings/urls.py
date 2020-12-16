9f6ca56b3bfd34b018a22b571149015d49378300
Yumei <yumei@YMacBook-Pro.local>
2020-12-16 14:11:59 -0500

    dynamic info display

--- listings/urls.py
+++ listings/urls.py
@@ -5,8 +5,8 @@ from . import views
 urlpatterns = [
 
     path('', views.index, name='listings'),
-    # path('<int:Listing_id>', views.listings, name='listing'),
-    path('listing', views.listing, name='listing'),
+    path('<int:listing_id>', views.listing, name='listing'),
+    # path('listing', views.listing, name='listing'),
     path('search', views.search, name='search'),
 ]
 
