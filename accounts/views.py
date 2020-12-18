a32cd99559fffc889acad291497dcba153cf2c2f
Yumei <yumei@YMacBook-Pro.local>
2020-12-18 16:17:54 -0500

    register and login done

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,76 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
 
 #view methods here:
 
 def register(request):
-    return render(request, 'accounts/register.html')
+    if request.method == 'POST': 
+
+    # Getform values
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+    # Check if passwords match
+        if password == password2:
+            # Check username for duplicates
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'That username is taken.')
+                return redirect('register')
+            else:
+            # Check email for duplicates
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, 'That email is in use.')
+                    return redirect('register')
+                else:
+                    #Looks good
+                    user = User.objects.create_user(
+                        username=username,
+                        password=password,
+                        email=email,
+                        first_name=first_name,
+                        last_name=last_name
+                    )
+                #Login after register -- need to add "auth" after 'import messages'
+                    # auth.login(request, user)
+                    # messages.success(request, 'You are now logged in')
+                    # return redirect('index')    
+                user.save()
+                messages.success(request, 'You are now registered. Please sign in.')
+        else:
+            messages.error(request, 'Passwords do not match')
+            return redirect('register')
+
+        return redirect('register')
+    else:
+        return render(request, 'accounts/register.html')
 
 def login(request):
-    return render(request, 'accounts/login.html')
+    if request.method == 'POST': 
+        #Login User
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'You are now logged in.')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Invalid credentials')
+            return render(request, 'accounts/login.html')
+
+        messages.error(request, 'Testing error')
+        #register User
+        # print('SUBMITTED REG')                #test message
+        return redirect('register')
+    else:      
+        return render(request, 'accounts/login.html')
 
 def logout(request):
     return redirect('index')
--- btre/settings.py
+++ btre/settings.py
@@ -142,3 +142,9 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'btre/static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+#Messages
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
